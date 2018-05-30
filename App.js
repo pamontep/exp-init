c77be225dd24a38383ebc6802d234d257bcbe4d5
Pamontep Panya <pamontep.p@gmail.com>
2018-05-30 16:59:04 +0700

    Edit some license

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /*
 The MIT License (MIT)
-copyright © 2015 <copyright holders>
+copyright © 2018 Pamontep Panya
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 documentation files (the “Software”), to deal in the Software without restriction, including without 
 limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
